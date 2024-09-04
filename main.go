f1a8ac93c60da00d2b82bc448e16f4432ca246eb
Magnus Andersson <mpja69@gmail.com>
2024-09-04 15:26:36 +0200

    assignmet 2 - fileserver

--- main.go
+++ main.go
@@ -1,14 +1,27 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type fileHandler struct {
+}
+
+func (h *fileHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {}
 
 func main() {
-	handler := http.NewServeMux()
-	server := http.Server{
-		Handler: handler,
-		Addr:    "8080",
+	port := "8080"
+	mux := http.NewServeMux()
+	srv := http.Server{
+		Handler: mux,
+		Addr:    ":" + port,
 	}
 
-	server.ListenAndServe()
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+
+	fmt.Printf("Starting server on %s\n", srv.Addr)
+	log.Fatal(srv.ListenAndServe())
 
 }
