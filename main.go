13a92f96f85a6b1a5fed021dc0f4f0a8d0e45124
Magnus Andersson <mpja69@gmail.com>
2024-09-06 16:38:44 +0200

    added wrapper func, for stateful counting

--- main.go
+++ main.go
@@ -6,17 +6,37 @@ import (
 	"net/http"
 )
 
+type apiConfig struct {
+	fileserverHits int
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits += 1
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits)))
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits = 0
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hits reset to 0"))
+}
+
 func main() {
 	fileRoot := "."
 	port := "8080"
-	mux := http.NewServeMux()
-	srv := &http.Server{
-		Handler: mux,
-		Addr:    ":" + port,
-	}
-
 	// Add a handler for files, starting in root
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir(fileRoot))))
+	// mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir(fileRoot))))
+	apiCfg := apiConfig{}
+	mux := http.NewServeMux()
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(fileRoot)))))
 
 	// Add a handleFunc for a specific path
 	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
@@ -25,6 +45,15 @@ func main() {
 		w.Write([]byte(http.StatusText(http.StatusOK)))
 
 	})
+
+	mux.HandleFunc("/reset", apiCfg.handlerReset)
+
+	mux.HandleFunc("/metrics", apiCfg.handlerMetrics)
+
+	srv := &http.Server{
+		Handler: mux,
+		Addr:    ":" + port,
+	}
 
 	fmt.Printf("Starting server on: %s, serving files from: %s\n", srv.Addr, fileRoot)
 	log.Fatal(srv.ListenAndServe())
